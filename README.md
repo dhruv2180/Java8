5620e18c323ba0b7e5c59df5ef893797d9600596
Dhruv Bhardwaj <dhruv2180@gmail.com>
2018-01-04 01:00:53 -0500

    add keyword file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Java8
+
+Code practise based on lecture by Venkat Subramaniam
+https://www.youtube.com/watch?v=j9nj5dTo54Q
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.util.*;
+import java.util.function.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Integer> values = Arrays.asList(1, 2, 3, 4, 5);
+
+        /*
+
+            Complex as manage variable, boundry conditions
+            uses external iterator
+         */
+
+        for (int i = 0; i < values.size(); i++) {
+
+            System.out.print(values.get(i));
+        }
+
+        /*
+            Simpler since lesser moving parts, but still complex since uses
+            external iterators
+
+         */
+
+        for (int i:values){
+            System.out.print(i);
+        }
+
+
+        /*
+            Internal iterator approach, management of iterator done by automatically
+            by the collection we focus on what we need to do per iteration
+
+            Looping  decided at runtime (polymorphism)
+         */
+
+        values.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer value) {
+                System.out.println(value);
+            }
+        });
     }
 }
