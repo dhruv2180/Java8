d87c27c9fd94c713aed5518fb92fe11972ba667f
Dhruv Bhardwaj <dhruv2180@gmail.com>
2018-01-06 23:57:16 -0500

    update

---
+++ src/FunctionalStyle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.function.Predicate;
+
+public class FunctionalStyle {
+
+    public static void findImperative(List<String> symbols){
+        List<StockInfo> stocks=new ArrayList();
+
+        for(String symbol:symbols){
+
+        }
+
+    }
+
+    public static void main(String args[]){
+
+        Timeit.code(()->findImperative(Tickers.symbol));
+
+    }
+
+}
